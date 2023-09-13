f7ea26124731e171b405eb4618e93d7d0098afea
Emerson <88614881+EmersonGoncalvesSilva@users.noreply.github.com>
2023-09-14 00:17:40 +0200

    Add files via upload

---
+++ Neural_Network_Multiclass_Classification/neural_network_softmax.py
@@ -0,0 +1,266 @@
+""" 
+Neural Nerwork
+==============
+
+Neural Network Algorithm.
+Features:
+---------
+    - Activation Functions Options: 
+        - `linear`: linear regression funtion
+        - `sigmoid`: Logistic regression function
+        - `ReLU`: Rectified linear unit
+        - `Softmax`: Softmax activation function for multi-class classification
+    - Optimization Algorithm: `Gradient Descent`
+    - Cost Function: `Log Loss` if output layer is `sigmoid`, `Mean Squared Error` otherwise.
+    - About the parementers initialization(`weights` and `bias`):
+        - Weights are initialized with value equal to 1. Bias are initalized with values equal to 0. 
+        
+Use cases:
+------------
+This Neural Network module can be used to train models for `Multi-class classification`.    
+        
+How to use:
+-----------
+    - Use class `NN_layer()` to build hidden layers for NN
+    - Wire the layers in the model using the class `NN()`. e.g. `nn_model = NN(layer_01, layer_02, layer_03)`
+    - train the model using `NN()` class `.train()` method. e.g. `nn_model.train(features, labels, alpha, )`
+
+"""
+
+
+
+
+
+import numpy as np
+
+# Class for a layer:
+
+class NN_Layer():
+    """ 
+    - Summary: Class used to create a hidden layer for a Neural Net.
+
+    - Args:
+        - layer (str): Name of the layer, e.g `'layer_01'`
+        - units (int): Number of perceptrons in the hidden layer.
+        - activation_func (str, optional): Activation function. Defaults to 'linear'.
+    """
+    def __init__(self, layer: str ,units: int, activation_func: str= 'linear') -> None:
+
+        self.units = units
+        self.act_func = activation_func
+        self.layer = layer
+        # Initialize the Attributes that will be computed in the NN Model:
+        self.w = None
+        self.b = None
+        self.a = None
+        self.dj_dw = None
+        self.dj_db = None
+        
+        match activation_func:
+            case 'sigmoid':
+                self.g = self.Sigmoid.g
+                self.dg_dz = self.Sigmoid.dg_dz
+                self.cost = self.Sigmoid.cost
+            case 'relu':
+                self.g = self.Relu.g
+                self.dg_dz = self.Relu.dg_dz
+                self.cost = self.Relu.cost
+            case 'linear':
+                self.g = self.Linear.g
+                self.dg_dz = self.Linear.dg_dz
+                self.cost = self.Linear.cost
+
+            case 'softmax':
+                self.g = self.Softmax.g
+                self.dj_dz = self.Softmax.dj_dz
+                self.cost = self.Softmax.cost
+    
+
+    class Sigmoid:
+        @staticmethod
+        def g(z: np.array):
+            return 1/(1+np.exp(-z))
+        @staticmethod
+        def dg_dz(z: np.array):
+            return (1/(1+np.exp(-z)))*(1- (1/(1+np.exp(-z))))
+        @staticmethod
+        def cost(y:np.array, y_hat:np.array, m):
+            return sum(1/m)*(-y*np.log(y_hat) - (1-y)*np.log(1-y_hat))
+    class Relu:
+        @staticmethod
+        def g(z: np.array):
+            return np.maximum(0, z)
+        @staticmethod
+        def dg_dz(z: np.array):
+            return np.where(z>0, 1, 0)
+        @staticmethod
+        def cost(y:np.array, y_hat:np.array, m):
+            return sum((1/m)*(y - y_hat)**2)
+    class Linear:
+        @staticmethod
+        def g(z: np.array):
+            return z
+        @staticmethod
+        def dg_dz(z: np.array):
+            return 1
+        @staticmethod
+        def cost(y:np.array, y_hat:np.array, m):
+            return sum((1/m)*(y - y_hat)**2)
+    
+    # DEFINITION OF THE SOFTMAX FUNCTION:
+    class Softmax:
+        @staticmethod
+        def g(z) ->np.array:
+            return np.exp(z)/np.sum(np.exp(z), axis=1).reshape(-1,1) # [e^z]/[sum(e^z)]
+    
+        ##
+        @staticmethod
+        def cost(y:np.array, y_hat:np.array, m):
+            return (1/m)*np.sum(-y*np.log(y_hat))                    # -y*ln(y_hat)/m 
+        ##
+        @staticmethod
+        def dj_dz(y: np.array, y_hat: np.array) ->np.array:
+            return y_hat - y
+
+
+
+    def __str__(self) -> str:
+        return f'Layer: {self.layer}'
+
+
+class NN():
+    """
+    Neural Network Model
+    --------------------
+    - Use this class to create a neural net model. 
+    - eg. `nn_model = NN( [NN_Layer('layer_01', 8), NN_Layer('layer_02', 8), ] ) `
+
+    """
+    def __init__(self, hidden_layers: list) -> None:
+        # NN Architecture: 
+        self.hidden_layers = hidden_layers
+        self.cost = None
+    ##
+
+    def train(self, features: np.array, labels: np.array, alpha: float= .001, n_iterations:int=10000, e:float=1e-4, print_cost=True, print_each:int=1000):
+        """
+        Training:
+        --------
+
+        - Args:
+            - features (np.array): X training data. `2-D array - (dataset_size x Nr features)`
+            - labels (np.array): Y training data `2-D array - (dataset_size x Nr labels)`
+            - alpha (float, optional): Learning rate used to update W,B. Defaults to .001.
+            - n_iterations (int, optional): Number of iterations. Defaults to 10000.
+            - e (float, optional): Model convergency treshold . Defaults to 1e-4.
+        """
+
+        #01- Setting Paremeters:
+        #=======================
+        self.features = features
+        self.labels = labels
+        self.m = self.features.shape[0]
+        self.alpha = alpha
+        self.n_iterations = n_iterations
+
+        #01.01- Initialize W and B Matrices:
+        a_in_shape1 = self.features.shape[1]                                # Nr of features in the a_in matrix.
+        for i, hidden_layer in enumerate(self.hidden_layers):
+            # Parameters w, b:
+            hidden_layer.w =  np.ones([a_in_shape1, hidden_layer.units])    # w.shape(a_in.shape[1], Nr_neurons)
+            hidden_layer.b = np.zeros([1, hidden_layer.units])               # b.shape(1, Nr_neurons)
+            a_in_shape1 = hidden_layer.units
+            
+
+        #02- LOOP FOR OPTIMIZATION - GRADIENT DESCENT:
+        #=============================================
+        cost_prev = 0 # to store the cost 
+        for _ in range(0, self.n_iterations):
+
+
+            #02.01 FORWARD PROPAGATION:
+            #===========================
+            a_in = self.features
+            for hidden_layer in self.hidden_layers:
+                hidden_layer.z = np.matmul(a_in, hidden_layer.w) + hidden_layer.b
+                hidden_layer.a = hidden_layer.g(hidden_layer.z)                 
+                a_in = hidden_layer.a           
+
+
+            #02.02 BACKWARD PROPAGATION:
+            #===========================
+
+            #02.02.01 At the Last Layer(Softmax Layer):
+            self.cost = self.hidden_layers[-1].cost(y=self.labels, y_hat=self.hidden_layers[-1].a, m= self.m)
+            if print_cost and _%print_each == 0:
+                print(self.cost)
+            if abs(self.cost - cost_prev) <= e:
+                if print_cost:
+                    print('Optimization has converged!')
+                    print(f'Iteration: \n\t{_}')
+                    print(f'Cost: \n\t{self.cost}')
+                break
+            cost_prev = self.cost
+
+
+            dj = self.hidden_layers[-1].dj_dz(y= self.labels, y_hat= self.hidden_layers[-1].a)        
+            
+            #dj = dj*da_dz
+            self.hidden_layers[-1].dj_db = np.sum(dj, axis=0).reshape(1,-1) # shape: (1, Nr_neurons)  
+            self.hidden_layers[-1].dj_dw = np.matmul(self.hidden_layers[-2].a.T, dj) 
+            w_pass = self.hidden_layers[-1].w
+
+            #02.02.02 Loop through the inner hidden layers:
+            for hidden_layer in reversed(self.hidden_layers[1:-1]):  
+                da_dz = hidden_layer.dg_dz(hidden_layer.z)                          # Activation function incorporated;
+                dj = np.matmul(dj, w_pass.T)
+                dj= dj*da_dz
+
+                w_pass = hidden_layer.w 
+
+                hidden_layer.dj_db = np.sum(dj, axis=0).reshape(hidden_layer.b.shape)  #  
+                # Get index of the previous layer:
+                i_prev_ly = self.hidden_layers.index(hidden_layer) - 1
+                hidden_layer.dj_dw = np.matmul(self.hidden_layers[i_prev_ly].a.T, dj) 
+
+            ##02.02.03 At the First Hidden Layer:
+            da_dz = self.hidden_layers[0].dg_dz(self.hidden_layers[0].z)
+            dj = np.matmul(dj, w_pass.T)
+            dj = dj*da_dz
+            self.hidden_layers[0].dj_db = np.sum(dj, axis=0).reshape(self.hidden_layers[0].b.shape)
+            self.hidden_layers[0].dj_dw = np.matmul(self.features.T, dj)
+
+
+            #02.03 UPDATE PARAMETERS:
+            #=========================
+
+            for hidden_layer in self.hidden_layers:
+                hidden_layer.b = hidden_layer.b - self.alpha*hidden_layer.dj_db
+                hidden_layer.w = hidden_layer.w - self.alpha*hidden_layer.dj_dw
+    ##
+        
+    def predict(self, x: np.array):
+        """_summary_
+
+        Args:
+            x (np.array): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        a_input = x
+        for hidden_layer in self.hidden_layers:
+            z = np.matmul(a_input, hidden_layer.w) + hidden_layer.b
+            # pass z in the init function:
+            a = hidden_layer.g(z)
+            #print(f'Shape of the array a: \n\t{hidden_layer.a.shape}') 
+            a_input = a
+        return a_input
+    
+
+##
+##
+
+# Testing:
+if __name__ == '__main__':
+    pass
---
+++ Neural_Network_Multiclass_Classification/readme.md
@@ -0,0 +1,181 @@
+# Neural Network for Multiclass Classification with Softmax - Implementation from Scratch
+
+## Overview
+
+This Python module provides a comprehensive implementation of a neural network for multiclass classification using the softmax activation function. This neural network is designed to take a set of input features and classify them into one of multiple classes. The softmax function is used to convert the raw output scores of the network into class probabilities.
+
+## Table of Contents
+
+- [Project Name](#project-name)
+  - [Table of Contents](#table-of-contents)
+  - [Getting Started](#getting-started)
+  - [Features](#features)
+  - [Concepts](#concepts)
+    - [Perceptron](#perceptron)
+    - [Activation Function](#Activation-Function)
+    - [Layers](Layers#)
+    - [Forward Propagation](#Forward-Propagation)
+    - [Cost function](#Cost-function)
+    - [Minimization of the Loss Optimization Gradient Descent](#Minimization-of-the-Loss-Optimization-Gradient-Descent)
+    - [Backward Propagation](#Backward-Propagation)
+    - [Derivatives](#Derivatives)
+  - [Acknowledgments](#acknowledgments)
+
+## Getting Started
+
+How to create a neural network for multiclass classification:
+```
+
+>>import neural_network_softmax
+
+# Create a hidden layer:
+>> layer_01 = neural_network_softmax.NN_Layer(layer='layer_01', units= 5, activation_func='relu')
+# Create final layer with softmax activation function
+>> layer_02 = neural_network_softmax.NN_Layer(layer='layer_04', units= 3, activation_func='softmax')
+
+# Create model and connect the layers:
+>> nn_class_model = neural_network_softmax.NN([layer_01, layer_02])
+
+# Train model:
+>> nn_class_model.train(features=X, labels=Y, alpha= .001)
+
+
+```
+
+
+## Features
+
+- Supports any number of input features and classes.
+- Uses the softmax activation function for multiclass classification.
+- Allows customization of hyperparameters like learning rate, batch size, and the number of hidden layers and neurons.
+- Dense hidden layers.
+- Activation Functions Options: 
+    - `linear`: linear regression funtion
+    - `sigmoid`: Logistic regression function
+    - `ReLU`: Rectified linear unit
+    - `Softmax`: Softmax activation function for multi-class classification
+- Optimization Algorithm: `Gradient Descent`
+- Cost Function: `Log loss with Cross entropy`.
+- About the parementers initialization(`weights` and `bias`):
+    - Weights are initialized with value equal to 1. Bias are initalized with values equal to 0. 
+
+
+## Concepts
+
+How does a `Artificial Neural Network with softmax neural network work`?
+
+### Perceptron
+Perceptrons are the building block of the neural network. <br>
+Given an input $x_i$ the perceptron will compute a new value $z_i$, as follows:
+$$
+z_i = w_i x_i + b
+$$
+Where $w_i$ is the weight paremeter, and $b$ is the bias. 
+### Activation Function
+The equation above is a simple linear transformation, and in order to incorporate allow the model to map non-linearity, activations functions are used in neural network.
+- General activation function:
+$$
+a_i = f(z_i)
+$$
+
+- Relu activation function.
+$$
+ReLU(z_i) = max(0, z_i)
+$$
+- Softmax activation function
+$$
+\text{Softmax}(z)_i = \frac{e^{z_i}}{\sum_{j=1}^{N} e^{z_j}}
+$$
+
+
+
+### Layers
+A stack of perceptrons that receive the same input and use the same activation function constitute a layer in the Neural net. 
+A neural net can have many layers.
+
+### Forward Propagation
+$$
+[X_0] -> [A_1]-> [A_2] -> ... -> [Y_{hat}]
+$$
+
+Forward propagation is the process of passing input data through the neural network to make predictions.
+1. Weighted Sum: Calculate the weighted sum of inputs for each neuron in a layer.
+
+2. Activation Function: Apply an activation function (e.g., ReLU) to the weighted sum to introduce non-linearity.
+
+3. Repeat: Repeat these steps for each layer, passing the output of one layer as input to the next.
+
+4. Output Layer: In the output layer, apply the Softmax function to obtain class probabilities.
+
+### Cost function
+Once the data goes through all the transformations in the layers of the neural net, the values in the output layer are confronted with the correct values the model should give. The difference between last layer output and the desired results is the model error. The error is calculated using the loss function. 
+For multiclass classification problems the loss function typically used is called Cross-Entropy Loss. The cross-entropy loss measures the dissimilarity between the predicted probabilities and the true distribution of the labels.
+
+For a single data point with true label $y$ and predicted probabilities $\hat{y}$, the cross-entropy loss (also known as the log loss) is calculated as follows:
+
+$$
+L(y, \hat{y}) = -\sum_{i=1}^{N} y_i \log(\hat{y}_i)
+$$
+
+### Minimization of the Loss Optimization Gradient Descent
+
+Ideally the model should have a loss ~ 0. Drive the loss value of the equation above to zero is a task of optimization.
+
+The output of the last layer depends of the paremeters $w$ and $b$ used in each perceptron. Thus minimize the Loss value means find the values of  $w$ and $b$  in each perceptron that leads the the minumum loss.
+
+This tak is done using calculus (derivatives). The objective is to derive the loss in respect to each  $w$ and $b$. The derivate of the loss in respect to the paremeters contains the direction in which they should be updated in order to decrease the loss.
+The algorithm to finding the derivatives and progressivelly update the paremeters in order to decrease the loss is called Gradient Descent.   
+
+### Backward Propagation
+In order to calculate the derivates of the loss in respect to each $w$ and $b$ the algorithm needs to propagates the calculation from the last layers, where the loss in calcualted, untill the first layer. All of this calcualtions are done used the chain rule of derivation from calculus. 
+This whole process is called back propagation.
+
+1. Compute Loss: Calculate the loss (e.g., cross-entropy loss) between predicted and actual values.
+
+2. Backpropagate Error: Compute the gradients of the loss with respect to the model's weights using the chain rule.
+
+3. Update Weights: Update the weights in the network using optimization algorithms like gradient descent.
+
+
+
+### Derivatives
+
+#### Main Derivatives to perform back Propagation Using Softmax Activation Function
+#### Derivative of Cross-Entropy Loss with Respect to Softmax Input ($(z_i$))
+
+In multiclass classification, we often use the cross-entropy loss to measure the dissimilarity between predicted class probabilities and the true class labels. Given the softmax activation function $(\sigma(z_i)$) for class $(i$) and the true class probability $(y_i$), the cross-entropy loss is defined as:
+
+$$ \mathcal{L}(y_i, \sigma(z_i)) = -y_i \log(\sigma(z_i)) $$
+
+Now, let's calculate the derivative of the loss $(\mathcal{L}$) with respect to the input $(z_i$) of the softmax activation function:
+
+Using the chain rule, we can express this derivative as:
+
+$$ \frac{d\mathcal{L}(y_i, \sigma(z_i))}{dz_i} = \frac{d\mathcal{L}(y_i, \sigma(z_i))}{d\sigma(z_i)} \cdot \frac{d\sigma(z_i)}{dz_i} $$
+
+First, we find the derivative of the loss with respect to \(\sigma(z_i)\):
+
+$$ \frac{d\mathcal{L}(y_i, \sigma(z_i))}{d\sigma(z_i)} = -\frac{y_i}{\sigma(z_i)} $$
+
+Next, we've already calculated the derivative of the softmax activation with respect to its input $(z_i$) as:
+
+$$ \frac{d\sigma(z_i)}{dz_i} = \sigma(z_i) \cdot (1 - \sigma(z_i)) $$
+
+Now, we can combine these results:
+
+$$ \frac{d\mathcal{L}(y_i, \sigma(z_i))}{dz_i} = -\frac{y_i}{\sigma(z_i)} \cdot \sigma(z_i) \cdot (1 - \sigma(z_i)) $$
+
+Simplifying further:
+
+$$ \frac{d\mathcal{L}(y_i, \sigma(z_i))}{dz_i} = -y_i \cdot (1 - \sigma(z_i)) $$
+
+So, the derivative of the cross-entropy loss with respect to the input $(z_i$) of the softmax activation function is:
+
+$$ \frac{d\mathcal{L}(y_i, \sigma(z_i))}{dz_i} = \sigma(z_i) - y_i $$
+
+
+
+## Acknowledgments
+
+It was very fun write this project and  exercise my skills in Machine Learning.
+
